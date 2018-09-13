492ea322614ed5c536a9ba4a861adbf3b180650f
Roman Iakobchuk <kaboom.ua@gmail.com>
2018-09-13 23:16:47 +0300

    add component ref

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { findDOMNode } from 'react-dom'
 import ArticleList from './components/article-list'
 import ArticlesChart from './components/articles-chart'
 import articles from './fixtures'
@@ -7,10 +8,20 @@ class App extends Component {
   render() {
     return (
       <div>
-        <ArticleList articles={articles} />
+        <ArticleList articles={articles} ref={this.setArticleListRef} />
         <ArticlesChart articles={articles} />
       </div>
     )
+  }
+
+  setArticleListRef = (ref) => {
+    console.log('---', ref, findDOMNode(ref))
+    /*
+        setTimeout(() => {
+            ref.toggleOpenItem(articles[0].id)
+            ref.forceUpdate()
+        }, 1000)
+*/
   }
 }
 
